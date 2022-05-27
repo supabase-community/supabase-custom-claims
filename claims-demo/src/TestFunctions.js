6dc2baa754b00608291532b4beb056323daf9044
Mark Burggraf <markb@mantisbible.com>
2022-05-27 07:14:56 -0700

    return notes for each button press

--- claims-demo/src/TestFunctions.js
+++ claims-demo/src/TestFunctions.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 
 const TestFunctions = ({session}) => {
     const [output, setOutput] = useState('')
+    const [notes, setNotes] = useState('')
     const [title, setTitle] = useState('')
     const get_my_claims = async () => {
         setOutput('Loading...')
@@ -10,11 +11,13 @@ const TestFunctions = ({session}) => {
         const { data, error } = await supabase.rpc('get_my_claims');
         if (error) console.error('get_my_claims error', error);
         else setOutput(JSON.stringify(data, null, 2));
+        setNotes('This calls the server function "get_my_claims()" and gets the claims from the current token at the server.')
     }
     const show_session = async () => {
         setOutput('Loading...')
         setTitle('session object')
         setOutput(JSON.stringify(session, null, 2));
+        setNotes('This displays the entire session object that was returned from "supabase.auth.onAuthStateChange".')
     }   
     const is_claims_admin = async () => {
         setOutput('Loading...')
@@ -22,6 +25,7 @@ const TestFunctions = ({session}) => {
         const { data, error } = await supabase.rpc('is_claims_admin');
         if (error) console.error('is_claims_admin error', error);
         else setOutput(JSON.stringify(data, null, 2));
+        setNotes('This calls the server function "is_claims_admin()" and returns true if the current token on teh server has the "claims_admin" claim.')
     } 
     const session_claims = async () => {
         setTitle('session_claims')
@@ -30,6 +34,7 @@ const TestFunctions = ({session}) => {
         } else {
             setOutput(JSON.stringify(session.user?.app_metadata, null, 2))
         }
+        setNotes('This returns the value of "app_metadata" (the claims) from the current session object (returned from "supabase.auth.onAuthStateChange").')
     }
 	return (
         <>
@@ -62,6 +67,7 @@ const TestFunctions = ({session}) => {
         <div>
             <div className="title">{title}</div>
             <pre>{output}</pre>
+            <div className='notes'><b>{title}:</b> {notes}</div>
         </div>
 
         <div className="title">
